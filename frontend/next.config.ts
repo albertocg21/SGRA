772df2ef5964916b72a05ba3a255c6f92b1106d9
Alberto <albertocarballogonzalez@alumno.ieselrincon.es>
2026-04-16 11:17:39 +0100

    Prueba para isard

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://10.2.156.230", "http://10.2.156.230:3000"],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
